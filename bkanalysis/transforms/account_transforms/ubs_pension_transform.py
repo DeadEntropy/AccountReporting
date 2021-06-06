16d537f77553aa125093ae86b5d465d0a75972e3
DeadEntropy <39316933+DeadEntropy@users.noreply.github.com>
2021-06-06 22:11:56 +0100

    Account For Capital Gain in Pension Funds

--- bkanalysis/transforms/account_transforms/ubs_pension_transform.py
+++ bkanalysis/transforms/account_transforms/ubs_pension_transform.py
@@ -1,5 +1,5 @@
 import configparser
-
+from datetime import datetime, timedelta
 import pandas as pd
 import glob
 import os
@@ -15,21 +15,90 @@ def can_handle(path_in, config):
     return set(df.columns) == set(expected_columns)
 
 
+def get_key(proportion, date):
+    return max([v for v in list(proportion.keys()) if date > pd.to_datetime(v)])
+
+
+def fund_price(df_unit_price, date, fund_name, recursif=0):
+    try:
+        if recursif == 0:
+            return list(df_unit_price[(df_unit_price['Unit Price Date'] == date) & (df_unit_price['Fund Name'] == fund_name)]['Unit Price'])[0]
+        elif recursif<5:
+            return list(df_unit_price[(df_unit_price['Unit Price Date'] == date - timedelta(days=recursif)) & (df_unit_price['Fund Name'] == fund_name)]['Unit Price'])[0]
+        else:
+            raise Exception(f'Failed with fallback on {date}\t{fund_name}')
+    except IndexError:
+        if recursif < 5:
+            return fund_price(df_unit_price, date, fund_name, recursif+1)
+        else:
+            raise Exception(f'Failed on {date}\t{fund_name}')
+
+
+def __to_float(s):
+    try:
+        return s.str.replace('£', '').str.replace(',', '').astype('float64')
+    except AttributeError:
+        return s
+
+
+def get_transaction(path_in, path_unit_price, proportion: {}, switch: {}):
+    df_transaction = pd.read_csv(path_in).fillna(0.0)
+    df_unit_price = pd.read_csv(path_unit_price).dropna()
+
+    df_transaction['Effective Date'] = pd.to_datetime(df_transaction['Effective Date'], format='%d/%m/%Y')
+    df_transaction['Amount'] = __to_float(df_transaction['Amount']).fillna(0.0)
+    df_transaction = df_transaction.set_index('Effective Date')
+    df_unit_price['Unit Price Date'] = pd.to_datetime(df_unit_price['Unit Price Date'], format='%d/%m/%Y')
+
+    for fund_name in df_unit_price["Fund Name"].unique():
+        df_transaction[f'Price {fund_name}'] = [fund_price(df_unit_price, date, fund_name) for date in df_transaction.index]
+        df_transaction[f'Unit {fund_name}'] = df_transaction[f'Amount'] / df_transaction[f'Price {fund_name}'] * [proportion[get_key(proportion, date)][fund_name] for date in df_transaction.index]
+
+    for k,v in switch.items():
+        df_transaction.loc[k, "Unit Global Equity (Voluntary)"] = v['Global Equity (Voluntary)']
+        df_transaction.loc[k, "Unit Lifestyle (Voluntary)"] = v['Lifestyle (Voluntary)']
+
+    for fund_name in df_unit_price["Fund Name"].unique():
+        df_transaction[f'Cumulated Unit {fund_name}'] = df_transaction[f'Unit {fund_name}'][::-1].cumsum()[::-1]
+    df_transaction['Total Value'] = df_transaction['Price Global Equity (Voluntary)'] * df_transaction['Cumulated Unit Global Equity (Voluntary)'] + df_transaction['Price Lifestyle (Voluntary)'] * df_transaction['Cumulated Unit Lifestyle (Voluntary)']
+    df_transaction['Cumulated Capital Gain'] = df_transaction['Total Value'] - df_transaction['Amount'][::-1].cumsum()[::-1]
+    df_transaction['Incremental Capital Gain'] = df_transaction['Cumulated Capital Gain'][::-1].rolling(window=2).apply(
+        lambda x: x.iloc[1] - x.iloc[0]).fillna(0.0)[::-1]
+
+    return df_transaction
+
+
+def simplify(df_transaction):
+    df_gain = df_transaction[['Transaction Type', 'Transaction Currency', 'Incremental Capital Gain']].reset_index().rename(
+        columns={'Incremental Capital Gain': 'Amount'})
+    df_gain['Transaction Type'] = 'Capital Gain'
+    df_transfer = df_transaction[['Transaction Type', 'Transaction Currency', 'Amount']].reset_index()
+    return df_transfer.append(df_gain).sort_values('Effective Date', ascending=False).reset_index(drop=True)
+
+
 def load(path_in, config):
-    df = pd.read_csv(path_in)
-    expected_columns = parse_list(config['expected_columns'])
-    assert set(df.columns) == set(expected_columns), f'Was expecting [{", ".join(expected_columns)}] but file columns ' \
-                                                     f'are [{", ".join(df.columns)}]. (Lloyds Mortgage)'
+
+    if 'path_unit_price' in config:
+        df = simplify(get_transaction(path_in,
+                                      config['path_unit_price'],
+                                      parse_list(config['proportion'], False),
+                                      parse_list(config['switch'], False)))
+    else:
+        df = pd.read_csv(path_in)
+        expected_columns = parse_list(config['expected_columns'])
+        assert set(df.columns) == set(expected_columns), f'Was expecting [{", ".join(expected_columns)}] but file columns ' \
+                                                         f'are [{", ".join(df.columns)}]. (Lloyds Mortgage)'
+        df["Effective Date"] = pd.to_datetime(df["Effective Date"], format='%d/%m/%Y')
 
     df_out = pd.DataFrame(columns=sd.target_columns)
 
-    df_out.Date = pd.to_datetime(df["Effective Date"], format='%d/%m/%Y')
-    df_out.Account = config['account_name']
     df_out.Currency = df["Transaction Currency"]
     df_out.Amount = df.Amount
+    df_out.Date = df["Effective Date"]
     df_out.Subcategory = df["Transaction Type"]
     df_out.Memo = df["Transaction Type"]
     df_out['AccountType'] = config['account_type']
+    df_out.Account = config['account_name']
 
     return df_out
 
