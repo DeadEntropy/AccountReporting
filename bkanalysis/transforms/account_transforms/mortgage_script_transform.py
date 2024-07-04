d777ff06143c06a0551597be89da3ac7d9a43151
DeadEntropy <39316933+DeadEntropy@users.noreply.github.com>
2024-07-04 10:36:22 -0400

    account for overpayments in mortgages

--- bkanalysis/transforms/account_transforms/mortgage_script_transform.py
+++ bkanalysis/transforms/account_transforms/mortgage_script_transform.py
@@ -13,6 +13,7 @@ from mortgage import Loan
 from bkanalysis.transforms.account_transforms import static_data as sd
 from bkanalysis.config import config_helper as ch
 
+### There is an inacuracy inthe handling of interest with overpayment
 
 def can_handle(path_in, config, *args):
     if not path_in.lower().endswith('json'):
@@ -51,22 +52,34 @@ def get_cashflows(data):
     interest = data['interest']
     term = data['term']
     principal = data['principal']
-    escrow = data['escrow']
+    escrow = data['escrow'] if 'escrow' in data else 0 
+    if escrow < 0:
+        raise ValueError(f'Mortgage escrow cannot be negative: {escrow}.')
     account_name = data['account_name']
     currency = data['currency']
     include_balance = data['include_balance'] if "include_balance" in data else False
+    overpayment = data['overpayment'] if 'overpayment' in data else 0
+    if overpayment < 0:
+        raise ValueError(f'Mortgage Overpayment cannot be negative: {overpayment}.')
+    payments_per_month = data['payments_per_month'] if 'payments_per_month' in data else 1
     
     days_alive = (datetime.now() - start_date).days
     months_alive = int(days_alive / (365.25/12))
 
     skip_periods = data['skip_periods'] + 1 if 'skip_periods' in data else 1
-    take_periods = data['take_periods'] + 1 if 'take_periods' in data else months_alive + 1
+    take_periods = data['take_periods'] if 'take_periods' in data else months_alive + 1
+    last_period = min(months_alive + 1, skip_periods + take_periods)
 
     loan = Loan(principal=principal, interest=interest, term=term)
 
-    dates = [start_date + relativedelta(months=i) for i in range(skip_periods, take_periods)]
-    payments = [float(l.payment) + escrow for l in loan.schedule()[skip_periods:take_periods]]
-    interests = [-float(l.interest) for l in loan.schedule()[skip_periods:take_periods]]
+    dates = [start_date + relativedelta(months=i) for i in range(skip_periods, last_period)]
+    payments = [float(l.payment) + escrow + overpayment for l in loan.schedule()[skip_periods:last_period]]
+    interests = [-float(l.interest) for l in loan.schedule()[skip_periods:last_period]]
+
+    if payments_per_month > 1:
+        dates = [x for y in [[d, d + relativedelta(weeks=2)] for d in dates] for x in y]
+        payments = [x for y in [[p/2, p/2] for p in payments] for x in y]
+        interests = [x for y in [[i/2, i/2] for i in interests] for x in y]
 
     df_balance = pd.DataFrame(columns=['Date','Account', 'Currency','Amount','Subcategory','Memo', 'AccountType'])
     if include_balance:
@@ -98,15 +111,17 @@ def get_cashflows(data):
     df_interest['AccountType'] = 'mortgage'
 
     df_escrow = pd.DataFrame(columns=['Date','Account', 'Currency','Amount','Subcategory','Memo', 'AccountType'])
-    df_escrow.Date = dates
-    df_escrow.Account = account_name
-    df_escrow.Currency = currency
-    df_escrow.Amount = -escrow
-    df_escrow.Subcategory = 'Escrow'
-    df_escrow.Memo = 'Mortgage Escrow - Flood Insurance'
-    df_escrow['AccountType'] = 'mortgage'
+    if escrow != 0:
+        df_escrow.Date = dates
+        df_escrow.Account = account_name
+        df_escrow.Currency = currency
+        df_escrow.Amount = -escrow
+        df_escrow.Subcategory = 'Escrow'
+        df_escrow.Memo = 'Mortgage Escrow - Flood Insurance'
+        df_escrow['AccountType'] = 'mortgage'
 
-    return pd.concat([df_balance, df_payment, df_interest, df_escrow], axis=0)
+    df_mtg = pd.concat([df_balance, df_payment, df_interest, df_escrow], axis=0)
+    return df_mtg[df_mtg.Date <= datetime.now()]
 
 def load_save(config):
     files = glob.glob(os.path.join(config['folder_in'], '*.json'))
