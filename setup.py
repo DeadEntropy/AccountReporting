218c2c4582d5cfe596f5426900bb1953f4239ea1
DeadEntropy <39316933+DeadEntropy@users.noreply.github.com>
2024-12-30 07:47:16 -0500

    updates setup and requirements

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
    name='bkanalysis',
-   version='0.1',
+   version='0.2',
    description='A module to aggregate and analyse bank accounts',
    packages=['bkanalysis'],
    url='https://github.com/DeadEntropy/AccountReporting',
-   install_requires=['pandas', 'numpy', 'matplotlib'], #external packages as dependencies
+   install_requires=['pandas', 'numpy', 'matplotlib', 'mortgage', 'yfinance', 'yahooquery', 'cachetools'], #external packages as dependencies
 )
